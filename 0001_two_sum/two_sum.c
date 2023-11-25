54f093bf2712770b25a8ebbe79a908a4e3f266ec
Melwan Chevassus <melwan.chevassus@epita.fr>
2023-11-25 15:20:45 +0100

    0001 two_sum: add two_sum.c

---
+++ 0001_two_sum/two_sum.c
@@ -0,0 +1,24 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int index = 0; index < numsSize; index++)
+    {
+        for (int other = 0; other < numsSize; other++)
+        {
+            if (index != other && (nums[index] + nums[other] == target))
+            {
+                int *return_array = malloc(sizeof(int) * 2);
+                return_array[0] = index;
+                return_array[1] = other;
+                *returnSize = 2;
+                return return_array;
+            }
+        }
+    }
+    return NULL;
+}
